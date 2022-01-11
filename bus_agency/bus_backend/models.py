412a0450bd48f6b7807e0e9454e33c95efafceba
Gustavo Barrios Araya <gustavo.barrios@sansano.usm.cl>
2022-01-11 01:04:17 -0300

    refactor(bus_backend): remove models that will not be used

--- bus_agency/bus_backend/models.py
+++ bus_agency/bus_backend/models.py
@@ -3,22 +3,7 @@ from django.contrib.auth.models import User
 from django.core.validators import (
     MinValueValidator,
     MaxValueValidator,
-    RegexValidator,
 )
-
-
-class Driver(models.Model):
-    user = models.OneToOneField(
-        User,
-        on_delete=models.CASCADE,
-        primary_key=True)
-
-
-class Administrator(models.Model):
-    user = models.OneToOneField(
-        User,
-        on_delete=models.CASCADE,
-        primary_key=True)
 
 
 class Station(models.Model):
@@ -32,7 +17,7 @@ class Route(models.Model):
 
 
 class Bus(models.Model):
-    driver = models.ForeignKey(Driver, on_delete=models.SET_NULL)
+    driver = models.ForeignKey(User, on_delete=models.SET_NULL)
 
 
 class Seat(models.Model):
@@ -50,21 +35,7 @@ class Trip(models.Model):
     departure_time = models.DateTimeField()
 
 
-class Passenger(models.Model):
-    first_name = models.CharField()
-    last_name = models.CharField()
-    phone_regex = RegexValidator(
-        regex=r'^(\+?56)?(\s?)(9)(\s?)\d{4}(\s?)\d{4}',
-        message="Número de télefono invalido.")
-    phone_number = models.CharField(validators=[phone_regex], max_length=15)
-    rut_regex = RegexValidator(
-        regex=r'^d{8}-\d{1}$',
-        message="Ingrese RUT sin puntos y con guión.")
-    rut = models.CharField(max_length=10)  # Formato: 00000000-0
-    email = models.EmailField()
-
-
 class Ticket(models.Model):
     trip = models.ForeignKey(Trip, on_delete=models.CASCADE)
     seat = models.ForeignKey(Seat, on_delete=models.CASCADE)
-    passenger = models.ForeignKey(Passenger, on_delete=models.CASCADE)
+    passenger = models.ForeignKey(User, on_delete=models.CASCADE)
