ab203df0c1de0162b11c7e86e464c06da6e7ae63
Gustavo Barrios Araya <gustavo.barrios@sansano.usm.cl>
2022-01-14 23:09:47 -0300

    refactor(bus_backend): change permissions to use django contrib auth system

--- bus_agency/bus_backend/permissions.py
+++ bus_agency/bus_backend/permissions.py
@@ -1,56 +1,109 @@
 from rest_framework import permissions
 
 
-class IsOwnerOrReadOnly(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-        # Read permissions are allowed for everyone
-        if request.method in permissions.SAFE_METHODS:
-            return True
-
-        # Write permissions are only allowed to the owner of the ticket.
-        return obj.passenger == request.user
-
-
-class IsAnonymousOrAdmin(permissions.BasePermission):
+class PassengerPermissions(permissions.BasePermission):
     def has_permission(self, request, view):
-        # Allow only unautenticathed users passenger registration
-        # or superuser
-        return request.user.is_anonymous or request.user.is_superuser
+        # Read permissions are allowed for manager or admin users
+        if request.method in permissions.SAFE_METHODS:
+            return (
+                request.user.groups.filter(name='Manager').exists()
+                or request.user.is_superuser
+            )
+        elif request.method == 'POST':
+            # Create is allowed only for anonymous or admin users
+            return request.user.is_anonymous or request.user.is_superuser
+        elif request.method == 'PUT':
+            if request.user.has_perm(f'bus_backend.change_user'):
+                return True
+        elif request.method == 'PATCH':
+            if request.user.has_perm(f'bus_backend.change_user'):
+                return True
+        elif request.method == 'DELETE':
+            if request.user.has_perm(f'bus_backend.delete_user'):
+                return True
+        else:
+            return False
 
-
-class IsAccountOwnerOrAdmin(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
+        # Read and Write permissions are allowed only
+        # for account owner or admin
         return obj == request.user or request.user.is_superuser
 
 
-class IsManager(permissions.BasePermission):
+class DriverPermissions(permissions.BasePermission):
     def has_permission(self, request, view):
-        return request.user.groups.filter(name='Manager').exists()
+        # Read permissions are allowed for driver, manager or admin users
+        if request.method in permissions.SAFE_METHODS:
+            return (
+                request.user.groups.filter(name='Manager').exists()
+                or request.user.groups.filter(name='Driver').exists()
+                or request.user.is_superuser
+            )
+        elif request.method == 'POST':
+            # Create is allowed only for manager or admin users
+            return (
+                request.user.groups.filter(name='Manager').exists()
+                or request.user.is_superuser
+            )
+        elif request.method == 'PUT':
+            if request.user.has_perm(f'bus_backend.change_user'):
+                return True
+        elif request.method == 'PATCH':
+            if request.user.has_perm(f'bus_backend.change_user'):
+                return True
+        elif request.method == 'DELETE':
+            if request.user.has_perm(f'bus_backend.delete_user'):
+                return True
+        else:
+            return False
+
+    def has_object_permission(self, request, view, obj):
+        # Write permissions are allowed only for account owner or superuser
+        return obj == request.user or request.user.is_superuser
 
 
-class IsManagerOrAdmin(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return (
-            request.user.groups.filter(name='Manager').exists()
-            or request.user.is_superuser
-        )
+class ModelPermissions(permissions.BasePermission):
+    model = None
 
-
-class IsManagerOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         # Read permissions are allowed for everyone
         if request.method in permissions.SAFE_METHODS:
             return True
+        elif request.method == 'POST':
+            if request.user.has_perm(f'bus_backend.add_{self.model}'):
+                return True
+        elif request.method == 'PUT':
+            if request.user.has_perm(f'bus_backend.change_{self.model}'):
+                return True
+        elif request.method == 'PATCH':
+            if request.user.has_perm(f'bus_backend.change_{self.model}'):
+                return True
+        elif request.method == 'DELETE':
+            if request.user.has_perm(f'bus_backend.delete_{self.model}'):
+                return True
+        else:
+            return False
 
-        # Write permissions are only allowed for a manager account
-        return request.user.groups.filter(name='Manager').exists()
+
+class StationPermissions(ModelPermissions):
+    model = 'station'
 
 
-class IsDriver(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return request.user.groups.filter(name='Driver').exists()
+class RoutePermissions(ModelPermissions):
+    model = 'route'
 
 
-class IsPassenger(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return request.user.groups.filter(name='Passenger').exists()
+class BusPermissions(ModelPermissions):
+    model = 'bus'
+
+
+class TripPermissions(ModelPermissions):
+    model = 'trip'
+
+
+class TicketPermissions(ModelPermissions):
+    model = 'ticket'
+
+    # Write permissions are allowed only for ticket owner or superuser
+    def has_object_permission(self, request, view, obj):
+        return obj.passenger == request.user or request.user.is_superuser
