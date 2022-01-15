b0b0f908af4c6373478c12728353f0f34b91e960
Gustavo Barrios Araya <gustavo.barrios@sansano.usm.cl>
2022-01-15 18:36:31 -0300

    refactor(bus_backend): change django test case to pytest

---
+++ bus_agency/bus_backend/conftest.py
@@ -0,0 +1,11 @@
+import pytest
+
+from django.core.management import call_command
+
+
+@pytest.fixture(scope='session')
+def django_db_setup(django_db_setup, django_db_blocker):
+    with django_db_blocker.unblock():
+        call_command('loaddata', 'permissions.json')
+        call_command('loaddata', 'groups.json')
+        call_command('loaddata', 'users.json')
--- bus_agency/bus_backend/test/test_passenger.py
+++ bus_agency/bus_backend/test/test_passenger.py
@@ -1,171 +1,101 @@
-from django.contrib.auth.models import User
+import pytest
+
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 from rest_framework import status
-from rest_framework.test import APITestCase
+from rest_framework.test import APIClient
 
 
-class PassengerViewSetTests(APITestCase):
-    fixtures = [
-        'users.json',
-        'permissions.json',
-        'groups.json',
-        ]
+@pytest.fixture
+def passenger_data():
+    return {
+        'username': 'test_passenger',
+        'first_name': 'Test',
+        'last_name': 'Registration',
+        'email': 'test@registration.cl',
+        'password': 'password123',
+    }
 
-    def test_get_passenger_list_as_anonymous_user(self):
-        url = reverse('passenger-list')
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
-    def test_get_passenger_list_as_passenger_user(self):
-        login = self.client.login(
-            username='passenger_1',
-            password='password123'
-            )
-        url = reverse('passenger-list')
-        response = self.client.get(url)
-        self.assertTrue(login)
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+@pytest.fixture
+def client():
+    return APIClient()
 
-    def test_get_passenger_list_as_driver_user(self):
-        login = self.client.login(username='driver', password='password123')
-        url = reverse('passenger-list')
-        response = self.client.get(url)
-        self.assertTrue(login)
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
-    def test_get_passenger_list_as_manager_user(self):
-        login = self.client.login(username='manager', password='password123')
-        url = reverse('passenger-list')
-        response = self.client.get(url)
-        self.assertTrue(login)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+@pytest.mark.django_db()
+def test_get_passenger_list_as_anonymous_user(client):
+    url = reverse('passenger-list')
+    response = client.get(url)
+    assert response.status_code == status.HTTP_403_FORBIDDEN
 
-    def test_get_passenger_list_as_admin_user(self):
-        login = self.client.login(username='admin', password='password123')
-        url = reverse('passenger-list')
-        response = self.client.get(url)
-        self.assertTrue(login)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_create_passenger_as_anonymous_user(self):
-        url = reverse('passenger-list')
-        data = {
-            'username': 'test_passenger',
-            'first_name': 'Test',
-            'last_name': 'Registration',
-            'email': 'test@registration.cl',
-            'password': 'password123',
-        }
-        response = self.client.post(url, data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(User.objects.count(), 6)
+@pytest.mark.django_db()
+@pytest.mark.parametrize(
+    "username, password, expected_status",
+    [
+        ('passenger_1', 'password123', status.HTTP_403_FORBIDDEN),
+        ('driver', 'password123', status.HTTP_403_FORBIDDEN),
+        ('manager', 'password123', status.HTTP_200_OK),
+        ('admin', 'password123', status.HTTP_200_OK),
+    ]
+)
+def test_get_passenger_list_as_authenticated_user(client, username, password, expected_status):
+    login = client.login(username=username, password=password)
+    url = reverse('passenger-list')
+    response = client.get(url)
+    assert login
+    assert response.status_code == expected_status
 
-    def test_create_passenger_as_passenger_user(self):
-        login = self.client.login(
-            username='passenger_1',
-            password='password123'
-            )
-        url = reverse('passenger-list')
-        data = {
-            'username': 'test_passenger',
-            'first_name': 'Test',
-            'last_name': 'Registration',
-            'email': 'test@registration.cl',
-            'password': 'password123',
-        }
-        response = self.client.post(url, data, format='json')
-        self.assertTrue(login)
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-        self.assertEqual(User.objects.count(), 5)
 
-    def test_create_passenger_as_driver_user(self):
-        login = self.client.login(username='driver', password='password123')
-        url = reverse('passenger-list')
-        data = {
-            'username': 'test_passenger',
-            'first_name': 'Test',
-            'last_name': 'Registration',
-            'email': 'test@registration.cl',
-            'password': 'password123',
-        }
-        response = self.client.post(url, data, format='json')
-        self.assertTrue(login)
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-        self.assertEqual(User.objects.count(), 5)
+@pytest.mark.django_db()
+def test_create_passenger_as_anonymous_user(client, passenger_data):
+    url = reverse('passenger-list')
+    response = client.post(url, passenger_data, format='json')
+    assert response.status_code == status.HTTP_201_CREATED
+    assert User.objects.count() == 6
 
-    def test_create_passenger_as_manager_user(self):
-        login = self.client.login(username='manager', password='password123')
-        url = reverse('passenger-list')
-        data = {
-            'username': 'test_passenger',
-            'first_name': 'Test',
-            'last_name': 'Registration',
-            'email': 'test@registration.cl',
-            'password': 'password123',
-        }
-        response = self.client.post(url, data, format='json')
-        self.assertTrue(login)
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-        self.assertEqual(User.objects.count(), 5)
 
-    def test_create_passenger_as_admin_user(self):
-        login = self.client.login(username='admin', password='password123')
-        url = reverse('passenger-list')
-        data = {
-            'username': 'test_passenger',
-            'first_name': 'Test',
-            'last_name': 'Registration',
-            'email': 'test@registration.cl',
-            'password': 'password123',
-        }
-        response = self.client.post(url, data, format='json')
-        self.assertTrue(login)
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(User.objects.count(), 6)
+@pytest.mark.django_db()
+@pytest.mark.parametrize(
+    "username, password, expected_status, expected_count",
+    [
+        ('passenger_1', 'password123', status.HTTP_403_FORBIDDEN, 5),
+        ('driver', 'password123', status.HTTP_403_FORBIDDEN, 5),
+        ('manager', 'password123', status.HTTP_403_FORBIDDEN, 5),
+        ('admin', 'password123', status.HTTP_201_CREATED, 6),
+    ]
+)
+def test_create_passenger_as_authenticated_user(client, passenger_data, username, password, expected_status, expected_count):
+    login = client.login(username=username, password=password)
+    url = reverse('passenger-list')
+    response = client.post(url, passenger_data, format='json')
+    assert login
+    assert response.status_code == expected_status
+    assert User.objects.count() == expected_count
 
-    def test_retrieve_passenger_as_anonymous_user(self):
-        url = reverse('passenger-detail', kwargs={'pk': 2})
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
-    def test_retrieve_passenger_as_account_owner(self):
-        login = self.client.login(
-            username='passenger_1',
-            password='password123'
-            )
-        url = reverse('passenger-detail', kwargs={'pk': 2})
-        response = self.client.get(url)
-        self.assertTrue(login)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+@pytest.mark.django_db()
+def test_retrieve_passenger_as_anonymous_user(client):
+    url = reverse('passenger-detail', kwargs={'pk': 2})
+    response = client.get(url)
+    assert response.status_code == status.HTTP_403_FORBIDDEN
 
-    def test_retrieve_passenger_as_another_passenger(self):
-        login = self.client.login(
-            username='passenger_2',
-            password='password123'
-            )
-        url = reverse('passenger-detail', kwargs={'pk': 2})
-        response = self.client.get(url)
-        self.assertTrue(login)
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
-    def test_retrieve_passenger_as_driver_user(self):
-        login = self.client.login(username='driver', password='password123')
-        url = reverse('passenger-detail', kwargs={'pk': 2})
-        response = self.client.get(url)
-        self.assertTrue(login)
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-
-    def test_retrieve_passenger_as_manager_user(self):
-        login = self.client.login(username='manager', password='password123')
-        url = reverse('passenger-detail', kwargs={'pk': 2})
-        response = self.client.get(url)
-        self.assertTrue(login)
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-
-    def test_retrieve_passenger_as_admin_user(self):
-        login = self.client.login(username='admin', password='password123')
-        url = reverse('passenger-detail', kwargs={'pk': 2})
-        response = self.client.get(url)
-        self.assertTrue(login)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+@pytest.mark.django_db()
+@pytest.mark.parametrize(
+    "username, password, expected_status",
+    [
+        ('passenger_1', 'password123', status.HTTP_200_OK),
+        ('passenger_2', 'password123', status.HTTP_403_FORBIDDEN),
+        ('driver', 'password123', status.HTTP_403_FORBIDDEN),
+        ('manager', 'password123', status.HTTP_403_FORBIDDEN),
+        ('admin', 'password123', status.HTTP_200_OK),
+    ]
+)
+def test_retrieve_passenger_as_authenticated_user(client, username, password, expected_status):
+    login = client.login(username=username, password=password)
+    url = reverse('passenger-detail', kwargs={'pk': 2})
+    response = client.get(url)
+    assert login
+    assert response.status_code == expected_status
