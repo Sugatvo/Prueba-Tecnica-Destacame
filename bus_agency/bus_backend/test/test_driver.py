6593c2ff5aa40fc457bc2fcec33d3419eacadf8b
Gustavo Barrios Araya <gustavo.barrios@sansano.usm.cl>
2022-01-16 01:44:52 -0300

    test(bus_backend): add tests for driver modelviewset actions

---
+++ bus_agency/bus_backend/test/test_driver.py
@@ -0,0 +1,255 @@
+import pytest
+
+from django.urls import reverse
+from django.contrib.auth.models import User
+
+from rest_framework import status
+from rest_framework.test import APIClient
+
+
+@pytest.fixture
+def driver_data():
+    return {
+        'username': 'driver_passenger',
+        'first_name': 'Driver',
+        'last_name': 'Registration',
+        'email': 'test@registration.cl',
+        'password': 'password123',
+    }
+
+
+@pytest.fixture
+def update_driver_data():
+    return {
+        'username': 'update_driver',
+        'first_name': 'Update',
+        'last_name': 'Driver',
+        'email': 'update@test.cl',
+        'password': 'new_password123'
+    }
+
+
+@pytest.fixture
+def partial_update_driver_data():
+    return {
+        'first_name': 'Partial',
+        'last_name': 'Update',
+    }
+
+
+@pytest.fixture
+def client():
+    return APIClient()
+
+
+@pytest.mark.django_db()
+def test_get_driver_list_as_anonymous_user(client):
+    url = reverse('driver-list')
+    response = client.get(url)
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+@pytest.mark.django_db()
+@pytest.mark.parametrize(
+    "username, password, expected_status",
+    [
+        ('passenger_1', 'password123', status.HTTP_403_FORBIDDEN),
+        ('driver_1', 'password123', status.HTTP_200_OK),
+        ('manager', 'password123', status.HTTP_200_OK),
+        ('admin', 'password123', status.HTTP_200_OK),
+    ]
+)
+def test_get_driver_list_as_authenticated_user(
+        client, username, password, expected_status):
+    login = client.login(username=username, password=password)
+    url = reverse('driver-list')
+    response = client.get(url)
+    assert login
+    assert response.status_code == expected_status
+
+
+@pytest.mark.django_db()
+def test_create_driver_as_anonymous_user(client, driver_data):
+    url = reverse('driver-list')
+    response = client.post(url, driver_data, format='json')
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+    assert User.objects.filter(groups__name='Driver').count() == 2
+
+
+@pytest.mark.django_db()
+@pytest.mark.parametrize(
+    "username, password, expected_status, expected_count",
+    [
+        ('passenger_1', 'password123', status.HTTP_403_FORBIDDEN, 2),
+        ('driver_1', 'password123', status.HTTP_403_FORBIDDEN, 2),
+        ('manager', 'password123', status.HTTP_201_CREATED, 3),
+        ('admin', 'password123', status.HTTP_201_CREATED, 3),
+    ]
+)
+def test_create_driver_as_authenticated_user(
+        client, driver_data, username,
+        password, expected_status, expected_count):
+    login = client.login(username=username, password=password)
+    url = reverse('driver-list')
+    response = client.post(url, driver_data, format='json')
+    assert login
+    assert response.status_code == expected_status
+    assert User.objects.filter(groups__name='Driver').count() == expected_count
+
+
+@pytest.mark.django_db()
+def test_retrieve_driver_as_anonymous_user(client):
+    url = reverse('driver-detail', kwargs={'pk': 5})
+    response = client.get(url)
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+@pytest.mark.django_db()
+@pytest.mark.parametrize(
+    "username, password, expected_status",
+    [
+        ('passenger_1', 'password123', status.HTTP_403_FORBIDDEN),
+        ('driver_1', 'password123', status.HTTP_200_OK),
+        ('driver_2', 'password123', status.HTTP_403_FORBIDDEN),
+        ('manager', 'password123', status.HTTP_403_FORBIDDEN),
+        ('admin', 'password123', status.HTTP_200_OK),
+    ]
+)
+def test_retrieve_driver_as_authenticated_user(
+        client, username, password, expected_status):
+    login = client.login(username=username, password=password)
+    url = reverse('driver-detail', kwargs={'pk': 5})
+    response = client.get(url)
+    assert login
+    assert response.status_code == expected_status
+
+
+@pytest.mark.django_db()
+def test_update_driver_as_anonymous_user(client, update_driver_data):
+    url = reverse('driver-detail', kwargs={'pk': 5})
+    response = client.put(url, update_driver_data, format='json')
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+@pytest.mark.django_db()
+@pytest.mark.parametrize(
+    "username, password",
+    [
+        ('passenger_1', 'password123'),
+        ('driver_2', 'password123'),
+        ('manager', 'password123'),
+    ]
+)
+def test_update_driver_as_authenticated_user_without_permissions(
+        client, update_driver_data, username, password):
+    login = client.login(username=username, password=password)
+    driver_1 = User.objects.get(username='driver_1')
+    url = reverse('driver-detail', kwargs={'pk': driver_1.id})
+    response = client.put(url, update_driver_data, format='json')
+    assert login
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+@pytest.mark.django_db()
+@pytest.mark.parametrize(
+    "username, password",
+    [
+        ('driver_1', 'password123'),
+        ('admin', 'password123'),
+    ]
+)
+def test_update_driver_as_authenticated_user_with_permissions(
+        client, update_driver_data, username, password):
+    login = client.login(username=username, password=password)
+    driver_1 = User.objects.get(username='driver_1')
+    url = reverse('driver-detail', kwargs={'pk': driver_1.id})
+    response = client.put(url, update_driver_data, format='json')
+    driver_1.refresh_from_db()
+    assert login
+    assert response.status_code == status.HTTP_200_OK
+    assert driver_1.username == 'update_driver'
+    assert driver_1.first_name == 'Update'
+    assert driver_1.last_name == 'Driver'
+    assert driver_1.email == 'update@test.cl'
+
+
+@pytest.mark.django_db()
+def test_partial_update_driver_as_anonymous_user(
+        client, partial_update_driver_data):
+    url = reverse('driver-detail', kwargs={'pk': 5})
+    response = client.patch(url, partial_update_driver_data, format='json')
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+@pytest.mark.django_db()
+@pytest.mark.parametrize(
+    "username, password",
+    [
+        ('passenger_2', 'password123'),
+        ('driver_2', 'password123'),
+        ('manager', 'password123'),
+    ]
+)
+def test_partial_update_driver_as_authenticated_user_without_permissions(
+        client, partial_update_driver_data, username, password):
+    login = client.login(username=username, password=password)
+    driver_1 = User.objects.get(username='driver_1')
+    url = reverse('driver-detail', kwargs={'pk': driver_1.id})
+    response = client.patch(url, partial_update_driver_data, format='json')
+    assert login
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+@pytest.mark.django_db()
+@pytest.mark.parametrize(
+    "username, password",
+    [
+        ('driver_1', 'password123'),
+        ('admin', 'password123'),
+    ]
+)
+def test_partial_update_driver_as_authenticated_user_with_permissions(
+        client, partial_update_driver_data, username, password):
+    login = client.login(username=username, password=password)
+    driver_1 = User.objects.get(username='driver_1')
+    url = reverse('driver-detail', kwargs={'pk': driver_1.id})
+    response = client.patch(url, partial_update_driver_data, format='json')
+    driver_1.refresh_from_db()
+    assert login
+    assert response.status_code == status.HTTP_200_OK
+    assert driver_1.username == 'driver_1'
+    assert driver_1.first_name == 'Partial'
+    assert driver_1.last_name == 'Update'
+    assert driver_1.email == 'driver_1@test.cl'
+
+
+@pytest.mark.django_db()
+def test_destroy_driver_as_anonymous_user(client):
+    url = reverse('driver-detail', kwargs={'pk': 5})
+    response = client.delete(url, format='json')
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+    assert User.objects.filter(groups__name='Driver').count() == 2
+
+
+@pytest.mark.django_db()
+@pytest.mark.parametrize(
+    "username, password, expected_status, expected_count",
+    [
+        ('passenger_1', 'password123', status.HTTP_403_FORBIDDEN, 2),
+        ('driver_1', 'password123', status.HTTP_204_NO_CONTENT, 1),
+        ('driver_2', 'password123', status.HTTP_403_FORBIDDEN, 2),
+        ('manager', 'password123', status.HTTP_403_FORBIDDEN, 2),
+        ('admin', 'password123', status.HTTP_204_NO_CONTENT, 1),
+    ]
+)
+def test_destroy_driver_as_authenticated_user(
+        client, username, password, expected_status, expected_count):
+    login = client.login(username=username, password=password)
+    driver_1 = User.objects.get(username='driver_1')
+    url = reverse('driver-detail', kwargs={'pk': driver_1.id})
+    response = client.delete(url, format='json')
+    assert login
+    assert response.status_code == expected_status
+    assert (
+        User.objects.filter(groups__name='Driver').count() == expected_count
+        )
--- bus_agency/bus_backend/test/test_passenger.py
+++ bus_agency/bus_backend/test/test_passenger.py
@@ -23,7 +23,7 @@ def update_passenger_data():
     return {
         'username': 'update_passenger',
         'first_name': 'Update',
-        'last_name': 'Test',
+        'last_name': 'Passenger',
         'email': 'update@test.cl',
         'password': 'new_password123'
     }
@@ -54,7 +54,7 @@ def test_get_passenger_list_as_anonymous_user(client):
     "username, password, expected_status",
     [
         ('passenger_1', 'password123', status.HTTP_403_FORBIDDEN),
-        ('driver', 'password123', status.HTTP_403_FORBIDDEN),
+        ('driver_1', 'password123', status.HTTP_403_FORBIDDEN),
         ('manager', 'password123', status.HTTP_200_OK),
         ('admin', 'password123', status.HTTP_200_OK),
     ]
@@ -73,17 +73,17 @@ def test_create_passenger_as_anonymous_user(client, passenger_data):
     url = reverse('passenger-list')
     response = client.post(url, passenger_data, format='json')
     assert response.status_code == status.HTTP_201_CREATED
-    assert User.objects.count() == 6
+    assert User.objects.filter(groups__name='Passenger').count() == 3
 
 
 @pytest.mark.django_db()
 @pytest.mark.parametrize(
     "username, password, expected_status, expected_count",
     [
-        ('passenger_1', 'password123', status.HTTP_403_FORBIDDEN, 5),
-        ('driver', 'password123', status.HTTP_403_FORBIDDEN, 5),
-        ('manager', 'password123', status.HTTP_403_FORBIDDEN, 5),
-        ('admin', 'password123', status.HTTP_201_CREATED, 6),
+        ('passenger_1', 'password123', status.HTTP_403_FORBIDDEN, 2),
+        ('driver_1', 'password123', status.HTTP_403_FORBIDDEN, 2),
+        ('manager', 'password123', status.HTTP_403_FORBIDDEN, 2),
+        ('admin', 'password123', status.HTTP_201_CREATED, 3),
     ]
 )
 def test_create_passenger_as_authenticated_user(
@@ -94,7 +94,9 @@ def test_create_passenger_as_authenticated_user(
     response = client.post(url, passenger_data, format='json')
     assert login
     assert response.status_code == expected_status
-    assert User.objects.count() == expected_count
+    assert (
+        User.objects.filter(groups__name='Passenger').count() == expected_count
+        )
 
 
 @pytest.mark.django_db()
@@ -110,7 +112,7 @@ def test_retrieve_passenger_as_anonymous_user(client):
     [
         ('passenger_1', 'password123', status.HTTP_200_OK),
         ('passenger_2', 'password123', status.HTTP_403_FORBIDDEN),
-        ('driver', 'password123', status.HTTP_403_FORBIDDEN),
+        ('driver_1', 'password123', status.HTTP_403_FORBIDDEN),
         ('manager', 'password123', status.HTTP_403_FORBIDDEN),
         ('admin', 'password123', status.HTTP_200_OK),
     ]
@@ -133,43 +135,43 @@ def test_update_passenger_as_anonymous_user(client, update_passenger_data):
 
 @pytest.mark.django_db()
 @pytest.mark.parametrize(
-    "username, password, expected_status",
+    "username, password",
     [
-        ('passenger_2', 'password123', status.HTTP_403_FORBIDDEN),
-        ('driver', 'password123', status.HTTP_403_FORBIDDEN),
-        ('manager', 'password123', status.HTTP_403_FORBIDDEN),
+        ('passenger_2', 'password123'),
+        ('driver_1', 'password123'),
+        ('manager', 'password123'),
     ]
 )
 def test_update_passenger_as_authenticated_user_without_permissions(
-        client, update_passenger_data, username, password, expected_status):
+        client, update_passenger_data, username, password):
     login = client.login(username=username, password=password)
     passenger_1 = User.objects.get(username='passenger_1')
     url = reverse('passenger-detail', kwargs={'pk': passenger_1.id})
     response = client.put(url, update_passenger_data, format='json')
     assert login
-    assert response.status_code == expected_status
+    assert response.status_code == status.HTTP_403_FORBIDDEN
 
 
 @pytest.mark.django_db()
 @pytest.mark.parametrize(
-    "username, password, expected_status",
+    "username, password",
     [
-        ('passenger_1', 'password123', status.HTTP_200_OK),
-        ('admin', 'password123', status.HTTP_200_OK),
+        ('passenger_1', 'password123'),
+        ('admin', 'password123'),
     ]
 )
 def test_update_passenger_as_authenticated_user_with_permissions(
-        client, update_passenger_data, username, password, expected_status):
+        client, update_passenger_data, username, password):
     login = client.login(username=username, password=password)
     passenger_1 = User.objects.get(username='passenger_1')
     url = reverse('passenger-detail', kwargs={'pk': passenger_1.id})
     response = client.put(url, update_passenger_data, format='json')
     passenger_1.refresh_from_db()
     assert login
-    assert response.status_code == expected_status
+    assert response.status_code == status.HTTP_200_OK
     assert passenger_1.username == 'update_passenger'
     assert passenger_1.first_name == 'Update'
-    assert passenger_1.last_name == 'Test'
+    assert passenger_1.last_name == 'Passenger'
     assert passenger_1.email == 'update@test.cl'
 
 
@@ -183,42 +185,40 @@ def test_partial_update_passenger_as_anonymous_user(
 
 @pytest.mark.django_db()
 @pytest.mark.parametrize(
-    "username, password, expected_status",
+    "username, password",
     [
-        ('passenger_2', 'password123', status.HTTP_403_FORBIDDEN),
-        ('driver', 'password123', status.HTTP_403_FORBIDDEN),
-        ('manager', 'password123', status.HTTP_403_FORBIDDEN),
+        ('passenger_2', 'password123', ),
+        ('driver_1', 'password123'),
+        ('manager', 'password123'),
     ]
 )
 def test_partial_update_passenger_as_authenticated_user_without_permissions(
-        client, partial_update_passenger_data,
-        username, password, expected_status):
+        client, partial_update_passenger_data, username, password):
     login = client.login(username=username, password=password)
     passenger_1 = User.objects.get(username='passenger_1')
     url = reverse('passenger-detail', kwargs={'pk': passenger_1.id})
     response = client.patch(url, partial_update_passenger_data, format='json')
     assert login
-    assert response.status_code == expected_status
+    assert response.status_code == status.HTTP_403_FORBIDDEN
 
 
 @pytest.mark.django_db()
 @pytest.mark.parametrize(
-    "username, password, expected_status",
+    "username, password",
     [
-        ('passenger_1', 'password123', status.HTTP_200_OK),
-        ('admin', 'password123', status.HTTP_200_OK),
+        ('passenger_1', 'password123'),
+        ('admin', 'password123'),
     ]
 )
 def test_partial_update_passenger_as_authenticated_user_with_permissions(
-        client, partial_update_passenger_data,
-        username, password, expected_status):
+        client, partial_update_passenger_data, username, password):
     login = client.login(username=username, password=password)
     passenger_1 = User.objects.get(username='passenger_1')
     url = reverse('passenger-detail', kwargs={'pk': passenger_1.id})
     response = client.patch(url, partial_update_passenger_data, format='json')
     passenger_1.refresh_from_db()
     assert login
-    assert response.status_code == expected_status
+    assert response.status_code == status.HTTP_200_OK
     assert passenger_1.username == 'passenger_1'
     assert passenger_1.first_name == 'Partial'
     assert passenger_1.last_name == 'Update'
@@ -226,22 +226,22 @@ def test_partial_update_passenger_as_authenticated_user_with_permissions(
 
 
 @pytest.mark.django_db()
-def test_destroy_passenger_as_anonymous_user(client, passenger_data):
+def test_destroy_passenger_as_anonymous_user(client):
     url = reverse('passenger-detail', kwargs={'pk': 2})
-    response = client.delete(url, passenger_data, format='json')
+    response = client.delete(url, format='json')
     assert response.status_code == status.HTTP_403_FORBIDDEN
-    assert User.objects.count() == 5
+    assert User.objects.filter(groups__name='Passenger').count() == 2
 
 
 @pytest.mark.django_db()
 @pytest.mark.parametrize(
     "username, password, expected_status, expected_count",
     [
-        ('passenger_1', 'password123', status.HTTP_204_NO_CONTENT, 4),
-        ('passenger_2', 'password123', status.HTTP_403_FORBIDDEN, 5),
-        ('driver', 'password123', status.HTTP_403_FORBIDDEN, 5),
-        ('manager', 'password123', status.HTTP_403_FORBIDDEN, 5),
-        ('admin', 'password123', status.HTTP_204_NO_CONTENT, 4),
+        ('passenger_1', 'password123', status.HTTP_204_NO_CONTENT, 1),
+        ('passenger_2', 'password123', status.HTTP_403_FORBIDDEN, 2),
+        ('driver_1', 'password123', status.HTTP_403_FORBIDDEN, 2),
+        ('manager', 'password123', status.HTTP_403_FORBIDDEN, 2),
+        ('admin', 'password123', status.HTTP_204_NO_CONTENT, 1),
     ]
 )
 def test_destroy_passenger_as_authenticated_user(
@@ -252,4 +252,6 @@ def test_destroy_passenger_as_authenticated_user(
     response = client.delete(url, format='json')
     assert login
     assert response.status_code == expected_status
-    assert User.objects.count() == expected_count
+    assert (
+        User.objects.filter(groups__name='Passenger').count() == expected_count
+        )
