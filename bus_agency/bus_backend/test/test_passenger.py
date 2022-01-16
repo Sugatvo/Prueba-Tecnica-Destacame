00fa4e6dbf121bcb1acc2da445baf8f05dd25006
Gustavo Barrios Araya <gustavo.barrios@sansano.usm.cl>
2022-01-15 22:27:37 -0300

    test(bus_backend): test update, partial_update and destroy actions in passenger modelviewset

--- bus_agency/bus_backend/test/test_passenger.py
+++ bus_agency/bus_backend/test/test_passenger.py
@@ -19,6 +19,25 @@ def passenger_data():
 
 
 @pytest.fixture
+def update_passenger_data():
+    return {
+        'username': 'update_passenger',
+        'first_name': 'Update',
+        'last_name': 'Test',
+        'email': 'update@test.cl',
+        'password': 'new_password123'
+    }
+
+
+@pytest.fixture
+def partial_update_passenger_data():
+    return {
+        'first_name': 'Partial',
+        'last_name': 'Update',
+    }
+
+
+@pytest.fixture
 def client():
     return APIClient()
 
@@ -40,7 +59,8 @@ def test_get_passenger_list_as_anonymous_user(client):
         ('admin', 'password123', status.HTTP_200_OK),
     ]
 )
-def test_get_passenger_list_as_authenticated_user(client, username, password, expected_status):
+def test_get_passenger_list_as_authenticated_user(
+        client, username, password, expected_status):
     login = client.login(username=username, password=password)
     url = reverse('passenger-list')
     response = client.get(url)
@@ -66,7 +86,9 @@ def test_create_passenger_as_anonymous_user(client, passenger_data):
         ('admin', 'password123', status.HTTP_201_CREATED, 6),
     ]
 )
-def test_create_passenger_as_authenticated_user(client, passenger_data, username, password, expected_status, expected_count):
+def test_create_passenger_as_authenticated_user(
+        client, passenger_data, username,
+        password, expected_status, expected_count):
     login = client.login(username=username, password=password)
     url = reverse('passenger-list')
     response = client.post(url, passenger_data, format='json')
@@ -93,9 +115,141 @@ def test_retrieve_passenger_as_anonymous_user(client):
         ('admin', 'password123', status.HTTP_200_OK),
     ]
 )
-def test_retrieve_passenger_as_authenticated_user(client, username, password, expected_status):
+def test_retrieve_passenger_as_authenticated_user(
+        client, username, password, expected_status):
     login = client.login(username=username, password=password)
     url = reverse('passenger-detail', kwargs={'pk': 2})
     response = client.get(url)
     assert login
     assert response.status_code == expected_status
+
+
+@pytest.mark.django_db()
+def test_update_passenger_as_anonymous_user(client, update_passenger_data):
+    url = reverse('passenger-detail', kwargs={'pk': 2})
+    response = client.put(url, update_passenger_data, format='json')
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+@pytest.mark.django_db()
+@pytest.mark.parametrize(
+    "username, password, expected_status",
+    [
+        ('passenger_2', 'password123', status.HTTP_403_FORBIDDEN),
+        ('driver', 'password123', status.HTTP_403_FORBIDDEN),
+        ('manager', 'password123', status.HTTP_403_FORBIDDEN),
+    ]
+)
+def test_update_passenger_as_authenticated_user_without_permissions(
+        client, update_passenger_data, username, password, expected_status):
+    login = client.login(username=username, password=password)
+    passenger_1 = User.objects.get(username='passenger_1')
+    url = reverse('passenger-detail', kwargs={'pk': passenger_1.id})
+    response = client.put(url, update_passenger_data, format='json')
+    assert login
+    assert response.status_code == expected_status
+
+
+@pytest.mark.django_db()
+@pytest.mark.parametrize(
+    "username, password, expected_status",
+    [
+        ('passenger_1', 'password123', status.HTTP_200_OK),
+        ('admin', 'password123', status.HTTP_200_OK),
+    ]
+)
+def test_update_passenger_as_authenticated_user_with_permissions(
+        client, update_passenger_data, username, password, expected_status):
+    login = client.login(username=username, password=password)
+    passenger_1 = User.objects.get(username='passenger_1')
+    url = reverse('passenger-detail', kwargs={'pk': passenger_1.id})
+    response = client.put(url, update_passenger_data, format='json')
+    passenger_1.refresh_from_db()
+    assert login
+    assert response.status_code == expected_status
+    assert passenger_1.username == 'update_passenger'
+    assert passenger_1.first_name == 'Update'
+    assert passenger_1.last_name == 'Test'
+    assert passenger_1.email == 'update@test.cl'
+
+
+@pytest.mark.django_db()
+def test_partial_update_passenger_as_anonymous_user(
+        client, partial_update_passenger_data):
+    url = reverse('passenger-detail', kwargs={'pk': 2})
+    response = client.patch(url, partial_update_passenger_data, format='json')
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+@pytest.mark.django_db()
+@pytest.mark.parametrize(
+    "username, password, expected_status",
+    [
+        ('passenger_2', 'password123', status.HTTP_403_FORBIDDEN),
+        ('driver', 'password123', status.HTTP_403_FORBIDDEN),
+        ('manager', 'password123', status.HTTP_403_FORBIDDEN),
+    ]
+)
+def test_partial_update_passenger_as_authenticated_user_without_permissions(
+        client, partial_update_passenger_data,
+        username, password, expected_status):
+    login = client.login(username=username, password=password)
+    passenger_1 = User.objects.get(username='passenger_1')
+    url = reverse('passenger-detail', kwargs={'pk': passenger_1.id})
+    response = client.patch(url, partial_update_passenger_data, format='json')
+    assert login
+    assert response.status_code == expected_status
+
+
+@pytest.mark.django_db()
+@pytest.mark.parametrize(
+    "username, password, expected_status",
+    [
+        ('passenger_1', 'password123', status.HTTP_200_OK),
+        ('admin', 'password123', status.HTTP_200_OK),
+    ]
+)
+def test_partial_update_passenger_as_authenticated_user_with_permissions(
+        client, partial_update_passenger_data,
+        username, password, expected_status):
+    login = client.login(username=username, password=password)
+    passenger_1 = User.objects.get(username='passenger_1')
+    url = reverse('passenger-detail', kwargs={'pk': passenger_1.id})
+    response = client.patch(url, partial_update_passenger_data, format='json')
+    passenger_1.refresh_from_db()
+    assert login
+    assert response.status_code == expected_status
+    assert passenger_1.username == 'passenger_1'
+    assert passenger_1.first_name == 'Partial'
+    assert passenger_1.last_name == 'Update'
+    assert not passenger_1.email
+
+
+@pytest.mark.django_db()
+def test_destroy_passenger_as_anonymous_user(client, passenger_data):
+    url = reverse('passenger-detail', kwargs={'pk': 2})
+    response = client.delete(url, passenger_data, format='json')
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+    assert User.objects.count() == 5
+
+
+@pytest.mark.django_db()
+@pytest.mark.parametrize(
+    "username, password, expected_status, expected_count",
+    [
+        ('passenger_1', 'password123', status.HTTP_204_NO_CONTENT, 4),
+        ('passenger_2', 'password123', status.HTTP_403_FORBIDDEN, 5),
+        ('driver', 'password123', status.HTTP_403_FORBIDDEN, 5),
+        ('manager', 'password123', status.HTTP_403_FORBIDDEN, 5),
+        ('admin', 'password123', status.HTTP_204_NO_CONTENT, 4),
+    ]
+)
+def test_destroy_passenger_as_authenticated_user(
+        client, username, password, expected_status, expected_count):
+    login = client.login(username=username, password=password)
+    passenger_1 = User.objects.get(username='passenger_1')
+    url = reverse('passenger-detail', kwargs={'pk': passenger_1.id})
+    response = client.delete(url, format='json')
+    assert login
+    assert response.status_code == expected_status
+    assert User.objects.count() == expected_count
