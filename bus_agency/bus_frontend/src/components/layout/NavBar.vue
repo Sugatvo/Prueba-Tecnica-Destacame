273bfdaf236811b37b79cbbe2ec3c4b934bf0399
Gustavo Barrios Araya <gustavo.barrios@sansano.usm.cl>
2022-01-18 18:20:57 -0300

    feat(bus_frontend): add login access to app bar

--- bus_agency/bus_frontend/src/components/layout/NavBar.vue
+++ bus_agency/bus_frontend/src/components/layout/NavBar.vue
@@ -1,11 +1,62 @@
 <template>
-    <div>  
-        <v-app-bar dense color="#1d4647" dark>
-            <v-tabs>
-                <v-tab>Home</v-tab>
-                <v-tab>About Us</v-tab>
-                <v-tab>Contact Us</v-tab>
-            </v-tabs>
-        </v-app-bar>
-    </div>  
+  <div>
+    <v-app-bar elevation="1" flat>
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+      <v-toolbar-title>
+        <router-link to="/" tag="span" style="cursor: pointer">
+          Destacame Bus
+        </router-link>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn outlined to="/login" color="black" tile large class="login">
+        <v-icon left>mdi-account-circle-outline</v-icon>
+        Acceder
+      </v-btn>
+    </v-app-bar>
+    <v-navigation-drawer v-model="drawer" absolute temporary>
+      <v-list nav dense>
+        <v-list-item-group
+          v-model="group"
+          active-class="deep-purple--text text--accent-4"
+        >
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-account</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Account</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-view-dashboard</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Dashboard</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    drawer: false,
+    group: null,
+  }),
+};
+</script>
+
+<style>
+.login.v-btn--active::before {
+  background-color: transparent;
+}
+
+</style>
